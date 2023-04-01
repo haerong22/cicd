2572c0a189646f2c9d20e15b88e52a6050a63764
haerong22 <haerong22@naver.com>
2023-04-01 18:21:45 +0900

    feat: ec2 생성 후 로컬 스크립트 원격 복사 후 실행

--- main.tf
+++ main.tf
@@ -8,35 +8,41 @@ resource "aws_instance" "example" {
 	vpc_security_group_ids = [aws_security_group.instance.id]
 	key_name = aws_key_pair.kp.key_name	
 
-	user_data = <<-EOF
-	#!/bin/bash
-	echo "Hello, World" > index.html
-	nohup busybox httpd -f -p 8080 &
-	EOF
-	
-	user_data_replace_on_change=true	
-
-	#connection {
-	#	type ="ssh"
-	#	host =self.public_ip
-  	#	user="ec2-user"
-  	#	private_key = tls_private_key.pk.private_key_pem 
- 	#}
-
 	provisioner "local-exec" {
-  		command = "echo ${self.public_ip} >> hosts"
+  		command = "echo ${self.public_ip} >> ./ansible/hosts"
   	}
  
-	provisioner "local-exec" {
-  		command = "sh start.sh"
-	}
+	#provisioner "local-exec" {
+  	#	command = "sh start.sh"
+	#}
 
-	#provisioner "remote-exec" {
-  	#	inline = [
-    	#		"chmod +x /tmp/start.sh",
-    	#		"/tmp/start.sh args",
-    	#	]
-  	#}
+        provisioner "file" {
+   	 	source = "script.sh"
+    		destination = "/home/ec2-user/script.sh"
+
+    		# 인스턴스에 SSH 연결
+    		connection {
+      			type        = "ssh"
+      			host        = self.public_ip
+      			user        = "ec2-user"
+      			private_key = tls_private_key.pk.private_key_pem
+ 		}	
+ 	}
+
+  	provisioner "remote-exec" {
+    		inline = [
+      			"chmod +x /home/ec2-user/script.sh",
+      			"/home/ec2-user/script.sh"
+    		]
+
+    		# 인스턴스에 SSH 연결
+    		connection {
+      			type        = "ssh"
+      			host        = self.public_ip
+      			user        = "ec2-user"
+      			private_key = tls_private_key.pk.private_key_pem
+    		}
+  	}
 
 	tags = {
 		Name = "terraform-example" 
